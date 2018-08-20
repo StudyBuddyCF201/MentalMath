1aeff70e91461c88cabe30bf7ef23df85e333d6e
Danul De Leon <danul.deleon@gmail.com>
2018-08-20 14:35:54 -0700

    added function to link to quiz page upon click

--- js/home.js
+++ js/home.js
@@ -1,19 +1,33 @@
 'use strict';
 
-var deckOne = document.getElementById('deck-one');
+// var deckOne = document.getElementById('deck-one');
+var addition = document.getElementById('addition');
 var nameForm = document.getElementById('usernameform');
-var username = ""
+var username = '';
+var quizSubject;
 
 nameForm.addEventListener('submit', function(event){
   event.preventDefault();
-  username = event.target.username.value
+  username = event.target.username.value;
   // console.log(event.target.username.value);
-})
+});
 
-deckOne.addEventListener('click', function(){
+// deckOne.addEventListener('click', function(){
+//   if(username){
+//     runQuiz();
+//   } else {
+//     alert('Please Enter a Username');
+//   }
+// });
+
+addition.addEventListener('click', function(){
   if(username){
     runQuiz();
   } else {
-    alert('Please Enter a Username')
+    alert('Please Enter a Username');
   }
-})
+});
+
+function runQuiz(){
+  window.location.href = 'quiz.html';
+}
