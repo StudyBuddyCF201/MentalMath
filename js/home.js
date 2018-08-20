13ae9d6fce72b83bee59cf88c0ab7d70d56cec23
Carlos Cadena <cs.cadena@gmail.com>
2018-08-20 15:39:05 -0700

    JSON is is being loaded when it exists, not showing form. JSON is being set for new user

--- js/home.js
+++ js/home.js
@@ -1,42 +1,41 @@
 'use strict';
 
-
-// var deckOne = document.getElementById('deck-one');
 var addition = document.getElementById('addition');
-var JSONpresent = JSON.parse(localStorage.getItem('user'));
+var subtraction = document.getElementById('subtraction');
+var division = document.getElementById('division');
+var disciplines = [addition, subtraction, division];
+var JSONpresent = JSON.parse(localStorage.getItem('User'));
 var nameForm = document.getElementById('usernameform');
 var username = '';
 var quizSubject;
 
 if (JSONpresent){
-  nameForm.setAttribute('style','display : none');
+  nameForm.setAttribute('style','display:none');
   var greet = document.getElementsByClassName('formOrGreeting');
   var hi = document.createElement('p');
-  hi.textContent = `Welocme back ${JSONpresent[0].username}`;
+  username = JSONpresent[0].userName
+  hi.textContent = `Welcome back ${JSONpresent[0].userName}`;
   greet.appendChild(hi);
 }
 
 nameForm.addEventListener('submit', function(event){
   event.preventDefault();
   username = event.target.username.value;
-  // console.log(event.target.username.value);
 });
 
-// deckOne.addEventListener('click', function(){
-//   if(username){
-//     runQuiz();
-//   } else {
-//     alert('Please Enter a Username');
-//   }
-// });
+for(var i = 0; i < disciplines.length; i++){
+  disciplines[i].addEventListener('click', function(){
+    if(username){
+      var me = new User(username)
+      localStorage.setItem('user', JSON.stringify(me))
+      runQuiz();
+    } else {
+      alert('Please Enter a Username');
+    }
+  });
+}
 
-addition.addEventListener('click', function(){
-  if(username){
-    runQuiz();
-  } else {
-    alert('Please Enter a Username');
-  }
-});
+
 
 function runQuiz(){
   window.location.href = 'quiz.html';
