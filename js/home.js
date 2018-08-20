cb1e56c7b02fdbdceafcf49d06977641e9dc3110
Andrew <andrewscottpowers@gmail.com>
2018-08-20 14:28:02 -0700

    changes index.html file to add sections

--- js/home.js
+++ js/home.js
@@ -1,8 +1,17 @@
 'use strict';
 
+var JSONpresent = JSON.parse(localStorage.getItem('user'));
 var deckOne = document.getElementById('deck-one');
 var nameForm = document.getElementById('usernameform');
 var username = ""
+
+if (JSONpresent){
+  nameForm.setAttribute('style','display : none');
+  var greet = document.getElementsByClassName('formOrGreeting');
+  var hi = document.createElement('p');
+  hi.textContent = `Welocme back ${JSONpresent[0].username}`;
+  greet.appendChild(hi);
+}
 
 nameForm.addEventListener('submit', function(event){
   event.preventDefault();
@@ -16,4 +25,4 @@ deckOne.addEventListener('click', function(){
   } else {
     alert('Please Enter a Username')
   }
-})
+}) 
