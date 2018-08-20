33ae268fb72d01b1992726d0f0c9b7c5352f3b0b
Carlos Cadena <cs.cadena@gmail.com>
2018-08-20 15:52:59 -0700

    variable fixes

--- js/home.js
+++ js/home.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var addition = document.getElementById('addition');
-var subtraction = document.getElementById('subtraction');
-var division = document.getElementById('division');
-var disciplines = [addition, subtraction, division];
+var additionEl = document.getElementById('addition');
+var subtractionEl = document.getElementById('subtraction');
+var divisionEl = document.getElementById('division');
+var disciplines = [additionEl, subtractionEl, divisionEl];
 var JSONpresent = JSON.parse(localStorage.getItem('User'));
 var nameForm = document.getElementById('usernameform');
 var username = '';
@@ -13,7 +13,7 @@ if (JSONpresent){
   nameForm.setAttribute('style','display:none');
   var greet = document.getElementsByClassName('formOrGreeting');
   var hi = document.createElement('p');
-  username = JSONpresent[0].userName
+  username = JSONpresent[0].userName;
   hi.textContent = `Welcome back ${JSONpresent[0].userName}`;
   greet.appendChild(hi);
 }
@@ -26,8 +26,8 @@ nameForm.addEventListener('submit', function(event){
 for(var i = 0; i < disciplines.length; i++){
   disciplines[i].addEventListener('click', function(){
     if(username){
-      var me = new User(username)
-      localStorage.setItem('user', JSON.stringify(me))
+      var me = new User(username);
+      localStorage.setItem('user', JSON.stringify(me));
       runQuiz();
     } else {
       alert('Please Enter a Username');
