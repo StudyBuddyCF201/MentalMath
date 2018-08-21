3d1cb57a58265f8608ad5aebcf2873fc6ab9e206
Andrew <andrewscottpowers@gmail.com>
2018-08-21 10:06:05 -0700

    got the results listing based local stol storage data

--- js/results.js
+++ js/results.js
@@ -9,6 +9,12 @@ resultOne.quizTime = currentTime.toDateString()
 resultOne.score = 7;
 results.push(resultOne);
 
+var resultTwo = new Result ('subtraction')
+resultTwo.quizTime = currentTime.toDateString()
+resultTwo.score = 9001;
+results.push(resultTwo);
+
+
 
 var newUser = new User('username', results)
 localStorage.setItem('User', JSON.stringify(newUser));
@@ -18,7 +24,9 @@ var userData = JSON.parse(localStorage.getItem('User'));
 var listOnPage = document.getElementById('load_scores');
 for(var i = 0; i < userData.results.length; i++){
   var resultList = document.createElement('li');
-  resultList.textContent = userData.results[i]['score'];
+  resultList.textContent = `Subject: ${userData.results[i]['subject']}, 
+  Score: ${userData.results[i]['score']},  
+  Date: ${userData.results[i]['quizTime']}`;
   console.log(userData.results);
   listOnPage.appendChild(resultList);
 }
